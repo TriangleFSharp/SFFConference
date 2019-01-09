f18fe3bc6f7702de05891bd4ed29904d7b7110c0
Eric Harding <eric.harding@bluetradesys.com>
2019-01-09 11:33:41 -0500

    Add some information to venue page.

--- src/Venue/Venue.fs
+++ src/Venue/Venue.fs
@@ -4,6 +4,7 @@ open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fable.Core.JsInterop
 open Fable.Core
+open Fulma
 
 [<AutoOpen>]
 module Leaflet =
@@ -33,6 +34,19 @@ module Leaflet =
   let popup children =
     ofImport "Popup" "react-leaflet" () children
 
+
+let hotel name link =
+  Media.media [] [
+    Media.left [] [
+      i [Class "fas fa-hotel"] []
+    ]
+    Media.content [] [
+      a [Href link] [
+        Heading.h5 [] [ str name ]
+      ]
+    ]
+  ]
+
 let view =
   let pos = 35.77502, -78.63811
   div [] [
@@ -46,4 +60,21 @@ let view =
         ]
       ]
     ]
+    Section.section [] [
+      Heading.h1 [] [
+        str "Venue"
+      ]
+      str "Southern Fried F# is being held at " 
+      a [ Href "https://www.redhat.com/en/about/videos/inside-red-hats-raleigh-headquarters"] [str "Red Hat global headquarters"]
+      str " in downtown Raleigh North Carolina."
+    ]
+    Section.section [] [
+      Heading.h3 [ Heading.IsSubtitle] [
+        str "Accommodation"
+      ]
+      hotel "Raleigh Marriott City Center" "https://www.google.com/maps/place/Raleigh+Marriott+City+Center/@35.774974,-78.6421814,17z/data=!4m5!3m4!1s0x89ac5f715804d045:0xf512109c25af5aea!8m2!3d35.7735791!4d-78.6399557"
+      hotel "Sheraton Raleigh" "https://www.google.com/maps/place/Sheraton+Raleigh+Hotel/@35.774974,-78.6421814,17z/data=!4m5!3m4!1s0x89ac5f717b90484d:0x91190ab641cc99a7!8m2!3d35.77497!4d-78.6399929"
+      hotel "Holiday Inn" "https://www.google.com/maps/place/Holiday+Inn+Raleigh+Downtown/@35.7769051,-78.6443635,16.25z/data=!4m5!3m4!1s0x89ac5f6f98fac369:0xebc5bc60334731e7!8m2!3d35.7810101!4d-78.644094"
+    ]
+    // Alternative Food?
   ]
