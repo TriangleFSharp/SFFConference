e6e4310a4ec49ead55eec8e2f74087a3be2a9612
vorotato <vash.arclan@gmail.com>
2019-01-12 23:09:45 -0500

    tried to make code more readable, also removed unused files.

--- src/Venue/Venue.fs
+++ src/Venue/Venue.fs
@@ -35,18 +35,72 @@ module Leaflet =
     ofImport "Popup" "react-leaflet" () children
 
 
-let hotel name link =
-  Media.media [] [
-    Media.left [] [
-      i [Class "fas fa-hotel"] []
+
+//these are terminal components, they don't take anything.
+let venue = 
+  Tile.parent [Tile.IsVertical] [
+    article [Class "tile is-child notification is-primary"] [
+      p [Class "title"] [ str "Venue" ]
+      Image.image [ Image.Is128x128 ] [
+        img [Src "https://www.redhat.com/profiles/rh/themes/redhatdotcom/img/logo.svg"]
+      ]
+      strong [] [
+        str "Southern Fried F#"
+      ]
+      str " is being held at " 
+      a [ Href "https://www.redhat.com/en/about/videos/inside-red-hats-raleigh-headquarters"] [str "Red Hat global headquarters"]
+      str " in downtown Raleigh North Carolina, just a few blocks from the Raleigh convention center."
     ]
-    Media.content [] [
-      a [Href link] [
-        str name
+  ]
+let food = 
+  Tile.parent [Tile.IsVertical] [
+    article [Class "tile is-child notification"] [
+      p [Class "title"] [ str "Food" ]
+      p [] [
+        // Dame's chicken and waffles
+        // Is this the right place to mention limited availability?
+        str "We'll have Chicken and Waffles as well as vegan and low carb options and there is a wide variety of choices within easy walking distance of the conference center."
       ]
     ]
   ]
 
+let accomodation = 
+  let hotel name link =
+    Media.media [] [
+      Media.left [] [
+        i [Class "fas fa-hotel"] []
+      ]
+      Media.content [] [
+        a [Href link] [
+          str name
+        ]
+      ]
+    ]
+
+  let hotels = 
+    p [] [
+            hotel 
+              "Raleigh Marriott City Center" 
+              "https://www.google.com/maps/place/Raleigh+Marriott+City+Center/@35.774974,-78.6421814,17z/data=!4m5!3m4!1s0x89ac5f715804d045:0xf512109c25af5aea!8m2!3d35.7735791!4d-78.6399557"
+            hotel 
+              "Sheraton Raleigh"
+              "https://www.google.com/maps/place/Sheraton+Raleigh+Hotel/@35.774974,-78.6421814,17z/data=!4m5!3m4!1s0x89ac5f717b90484d:0x91190ab641cc99a7!8m2!3d35.77497!4d-78.6399929"
+            hotel 
+              "Holiday Inn"
+              "https://www.google.com/maps/place/Holiday+Inn+Raleigh+Downtown/@35.7769051,-78.6443635,16.25z/data=!4m5!3m4!1s0x89ac5f6f98fac369:0xebc5bc60334731e7!8m2!3d35.7810101!4d-78.644094"
+          ]
+
+  Tile.parent [Tile.IsVertical] [
+    article [Class "tile is-child notification is-info"] [
+      p [Class "title"] [
+        str "Accomodation"
+      ]
+      p [Class "subtitle"] [
+        str "There are several options within easy walking distance."
+      ]
+      hotels
+    ]
+  ]
 
 let view =
   let pos = 35.77502, -78.63811
@@ -65,51 +119,9 @@ let view =
       Tile.ancestor [] [
         Tile.tile [Tile.IsVertical; Tile.Size Tile.Is12] [
           Tile.tile [] [
-            Tile.parent [Tile.IsVertical] [
-              article [Class "tile is-child notification is-primary"] [
-                p [Class "title"] [ str "Venue" ]
-                Image.image [ Image.Is128x128 ] [
-                  img [Src "https://www.redhat.com/profiles/rh/themes/redhatdotcom/img/logo.svg"]
-                ]
-                strong [] [
-                  str "Southern Fried F#"
-                ]
-                str " is being held at " 
-                a [ Href "https://www.redhat.com/en/about/videos/inside-red-hats-raleigh-headquarters"] [str "Red Hat global headquarters"]
-                str " in downtown Raleigh North Carolina, just a few blocks from the Raleigh convention center."
-              ]
-            ]
-            // Tile.parent [Tile.IsVertical] [
-            //   article [Class "tile is-child notification"] [
-            //     p [Class "title"] [ str "Food" ]
-            //     p [] [
-            //       // Dame's chicken and waffles
-            //       // Is this the right place to mention limited availability?
-            //       str "Stay for the chicken and waffles or, if that's not your style, there is a wide variety of choices within easy walking distance of the conference center."
-            //     ]
-            //   ]
-            // ]
-            Tile.parent [Tile.IsVertical] [
-              article [Class "tile is-child notification is-info"] [
-                p [Class "title"] [
-                  str "Accomodation"
-                ]
-                p [Class "subtitle"] [
-                  str "There are several options within easy walking distance."
-                ]
-                p [] [
-                  hotel 
-                    "Raleigh Marriott City Center" 
-                    "https://www.google.com/maps/place/Raleigh+Marriott+City+Center/@35.774974,-78.6421814,17z/data=!4m5!3m4!1s0x89ac5f715804d045:0xf512109c25af5aea!8m2!3d35.7735791!4d-78.6399557"
-                  hotel 
-                    "Sheraton Raleigh"
-                    "https://www.google.com/maps/place/Sheraton+Raleigh+Hotel/@35.774974,-78.6421814,17z/data=!4m5!3m4!1s0x89ac5f717b90484d:0x91190ab641cc99a7!8m2!3d35.77497!4d-78.6399929"
-                  hotel 
-                    "Holiday Inn"
-                    "https://www.google.com/maps/place/Holiday+Inn+Raleigh+Downtown/@35.7769051,-78.6443635,16.25z/data=!4m5!3m4!1s0x89ac5f6f98fac369:0xebc5bc60334731e7!8m2!3d35.7810101!4d-78.644094"
-                ]
-              ]
-            ]
+            venue
+            food
+            accomodation
           ]
         ]
       ]
