fa4382dca297f3086410fcdf18bac3517e1d9389
Alan Ball <vash.arclan@gmail.com>
2019-03-12 01:52:39 -0400

    removed call for speakers link

--- src/App.fs
+++ src/App.fs
@@ -38,9 +38,7 @@ let navBar =
   Navbar.navbar [Navbar.Color IsPrimary; Navbar.Option.Props [Role "navigation"]] [ 
     Navbar.Item.a  [ Navbar.Item.Props [Href "#home" ]] [ str "Home" ]
     Navbar.Item.a [ Navbar.Item.Props[Href "#speakers"]  ] [ str "Speakers" ]
-    //Navbar.Item.a [ Navbar.Item.Props[Href "#schedule"]  ] [ str "Schedule" ]
     Navbar.Item.a [ Navbar.Item.Props[Href "https://www.eventbrite.com/e/southern-fried-f-tickets-54591291021"]  ] [ str "Tickets" ]
-    Navbar.Item.a [ Navbar.Item.Props[Href "https://sessionize.com/southern-fried-fsharp"]  ] [ str "Call for Speakers" ]
     Navbar.Item.a [ Navbar.Item.Props[Href "#venue"]  ] [str "Venue" ]
     Navbar.End.div [] [
       Navbar.Item.div [ ] [
