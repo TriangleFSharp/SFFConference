36baae0a37da575d9da75409811d24e2752d0428
Alan Ball <vash.arclan@gmail.com>
2019-04-12 10:42:20 -0400

    fixed erics talk title

--- src/Schedule/Schedule.fs
+++ src/Schedule/Schedule.fs
@@ -13,10 +13,10 @@ let talks =  [
     {speaker="Stachu Korick"; title="Grappling the JavaScript Ecosystem with F# and Fable";  duration= hour}
     {speaker="James Novino"; title="Abstracting IO using F#";  duration= hour}
     {speaker=""; title="Lunch"; duration=TimeSpan(0,45,0)}
-    {speaker="Brett Rowberry"; title="Creating a Unit Conversion Library"; duration=TimeSpan(0,15,0)}
+    {speaker="Brett Rowberry"; title="Creating a Unit Conversion Library ⚡"; duration=TimeSpan(0,15,0)}
     {speaker="Olya Samusik"; title="Solving 3D Problems With F#";  duration= hour}
     {speaker="Luis Quintanilla"; title="Getting Started with F# in Jupyter Notebooks";  duration= hour}
-    {speaker="Eric Harding"; title="Getting Started with F# in Jupyter Notebooks";  duration= hour}
+    {speaker="Eric Harding"; title="Authoring React Components with Fable";  duration= hour}
     {speaker="Marlon Vilorio"; title="From C# to F#";  duration= hour}
 ]
 
@@ -28,17 +28,21 @@ let scheduleItem title speaker time =
         ]
 let scheduleHeader =
     thead [] [
-        tr [][th [][str "Title"]]
-        tr [][th [][str "Speaker"]]
-        tr [][th [][str "Time"]]
+        tr [][
+            th [][str "Title"]
+            th [][str "Speaker"]
+            th [][str "Time"]
+        ]
     ]
 let rec buildSchedule talks (startTime: DateTime)= 
     match talks with
     | [] -> []
     | {speaker=speaker; title=title; duration=duration} :: t -> 
-        (scheduleItem title speaker (startTime.ToShortTimeString()) ):: buildSchedule t (startTime + duration)
+        scheduleItem title speaker (startTime.ToShortTimeString()) :: buildSchedule t (startTime + duration)
 let view =
-    Table.table [] [
-        scheduleHeader
-        tbody [] (buildSchedule talks (DateTime(4,13,19,9,0,0,DateTimeKind.Local)))
+    Section.section [] [
+        Table.table [Table.IsStriped; Table.IsBordered; Table.IsHoverable] [
+            scheduleHeader
+            tbody [] (buildSchedule talks (DateTime(4,13,19,9,0,0,DateTimeKind.Local)))
+        ]
     ]
